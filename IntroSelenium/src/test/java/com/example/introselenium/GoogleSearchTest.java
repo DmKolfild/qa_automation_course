c9632092dc9a78b8a1138629d1764654e27dbf78
DmKolfild <dmitriybaranov0@gmail.com>
2023-05-03 23:52:22 +0300

    Init Selenium project
    Add tests for google.com and bing.com. Create reports for these tests

---
+++ IntroSelenium/src/test/java/com/example/introselenium/GoogleSearchTest.java
@@ -0,0 +1,48 @@
+package com.example.introselenium;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GoogleSearchTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.bing.com/");
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void search() {
+        String input = "Selenium";
+
+        WebElement searchField = driver.findElement(By.cssSelector("[type='search']"));
+        searchField.sendKeys(input);
+        searchField.submit();
+
+        WebElement searchPageField = driver.findElement(By.cssSelector("[type='search']"));
+        assertEquals(input, searchPageField.getAttribute("value"));
+    }
+
+}
