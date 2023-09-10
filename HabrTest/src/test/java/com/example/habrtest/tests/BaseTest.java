8c17ab70f56250be8d760a39406e78c1aa43fd7e
DmKolfild <dmitriybaranov0@gmail.com>
2023-09-10 14:52:51 +0300

    Review code

--- HabrTest/src/test/java/com/example/habrtest/tests/BaseTest.java
+++ HabrTest/src/test/java/com/example/habrtest/tests/BaseTest.java
@@ -1,6 +1,5 @@
 package com.example.habrtest.tests;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
